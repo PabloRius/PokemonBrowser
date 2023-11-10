861b0daff1643efa7a723bb3221a9225f33d2ccd
pgarcriu <100428976@alumnos.uc3m.es>
2023-11-10 13:01:59 +0100

    Hud and player placed along the screen

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -1,5 +1,32 @@
 class Battle {
-  constructor() {}
+  constructor() {
+    this.combatants = {
+      player1: new Combatant(
+        {
+          ...window.Pokemons.dragonite,
+          team: "player",
+          hp: 50,
+          maxHp: 50,
+          xp: 0,
+          level: 1,
+          status: null,
+        },
+        this
+      ),
+      enemy1: new Combatant(
+        {
+          ...window.Pokemons.dragonite,
+          team: "enemy",
+          hp: 50,
+          maxHp: 50,
+          xp: 0,
+          level: 1,
+          status: null,
+        },
+        this
+      ),
+    };
+  }
 
   createElement() {
     this.element = document.createElement("div");
@@ -18,5 +45,11 @@ class Battle {
   init(container) {
     this.createElement();
     container.appendChild(this.element);
+
+    Object.keys(this.combatants).forEach((key) => {
+      let combatant = this.combatants[key];
+      combatant.id = key;
+      combatant.init(this.element);
+    });
   }
 }
---
+++ content/pokemons.js
@@ -0,0 +1,44 @@
+window.PokemonTypes = {
+  bug: "bug",
+  dark: "dark",
+  dragon: "dragon",
+  electric: "electric",
+  fairy: "fairy",
+  fighting: "fighting",
+  fire: "fire",
+  flying: "flying",
+  ghost: "ghost",
+  grass: "grass",
+  ground: "ground",
+  ice: "ice",
+  normal: "normal",
+  poison: "poison",
+  psychic: "psychic",
+  rock: "rock",
+  steel: "steel",
+  water: "water",
+};
+
+window.Pokemons = {
+  bulbasaur: {
+    id: "001",
+    name: "Bulbasaur",
+    type: window.PokemonTypes.grass,
+    src: "assets/battle/pokes/bulbasaur.png",
+    icon: "assets/icons/pokes/bulbasaur.png",
+  },
+  charmander: {
+    id: "004",
+    name: "Charmander",
+    type: window.PokemonTypes.fire,
+    src: "assets/battle/pokes/charmander.png",
+    icon: "assets/icons/pokes/charmander.png",
+  },
+  dragonite: {
+    id: "149",
+    name: "Dragonite",
+    type: window.PokemonTypes.dragon,
+    src: "assets/battle/pokes/dragonite.png",
+    icon: "assets/icons/pokes/dragonite.png",
+  },
+};
